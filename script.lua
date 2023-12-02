8995bd4cb29cc655ce7db99a39a2f13bbbc46ce4
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-12-03 00:17:20 +0100

    Don't overwrite the default 'config' variable

--- script.lua
+++ script.lua
@@ -1,11 +1,11 @@
-local config = {
+local modelparts = {
     expression = models.model.root.Body.expression,
     cat_accessories = models.Extra.cat.cat_accessories,
     blahaj = models.External.blahaj.blahaj,
 }
 
 function setExpression(expression)
-    config.expression:setPrimaryTexture("CUSTOM", textures["Expressions." .. expression])
+    modelparts.expression:setPrimaryTexture("CUSTOM", textures["Expressions." .. expression])
 end
 
 function events.RENDER()
@@ -23,8 +23,8 @@ vanilla_model.PLAYER:setVisible(false)
 vanilla_model.ARMOR:setVisible(false)
 vanilla_model.CAPE:setVisible(false)
 -- Make custom model parts invisible
-config.cat_accessories:setVisible(false)
-config.blahaj:setVisible(false)
+modelparts.cat_accessories:setVisible(false)
+modelparts.blahaj:setVisible(false)
 
 
 local mainPage = action_wheel:newPage()
