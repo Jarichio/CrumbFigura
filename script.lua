9c9bd5cc40b7c6d25369016e3ab06554f5918575
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-12-02 23:57:14 +0100

    Add emotion support as well as reorganise the cat ears/tail, also add a player head plush

--- script.lua
+++ script.lua
@@ -1,7 +1,32 @@
+local config = {
+    expression = models.model.root.Body.expression,
+    cat_accessories = models.Extra.cat.cat_accessories,
+    blahaj = models.External.blahaj.blahaj,
+}
+
+function setExpression(expression)
+    config.expression:setPrimaryTexture("CUSTOM", textures[expression])
+end
+
+function events.RENDER()
+    -- Reset the expression in case nothing else is setting it
+    setExpression("Expressions.NeutralExpression")
+
+    local isEating = player:getActiveItem():getUseAction() == "EAT"
+    local isEatingBread = isEating and player:getActiveItem().id == "minecraft:bread"
+    if isEatingBread then setExpression("Expressions.CryingExpression") end
+    
+    -- log(textures.crumb:getName())
+end
+
+-- Make vanilla model parts invisible
 vanilla_model.PLAYER:setVisible(false)
 vanilla_model.ARMOR:setVisible(false)
+vanilla_model.CAPE:setVisible(false)
+-- Make custom model parts invisible
+config.cat_accessories:setVisible(false)
+config.blahaj:setVisible(false)
 
-function events.tick()
-    --log(player:isUsingItem(true))
-    log(textures.crumb:getName())
-end
+
+local mainPage = action_wheel:newPage()
+action_wheel:setPage(mainPage)
